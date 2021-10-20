8c7f009db4376fd49121df11b65616795bd73020
abu_lorianz <50519242+abulorianz@users.noreply.github.com>
2021-10-20 18:16:48 +0300

    Add files via upload

---
+++ CustomCtrl/CustomCtrl/FemtoHelpTextBox.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomCtrl.CustomCtrl
+{
+    [DefaultEvent("_TextChanged")]
+    public partial class FemtoHelpTextBox : UserControl
+    {
+        public FemtoHelpTextBox()
+        {
+            InitializeComponent();
+        }
+        //Fields
+        private Color borderColor = Color.MediumSlateBlue;
+        private int borderSize = 2;
+        private bool underlinedStyle = false;
+        private Color borderFocusColor = Color.HotPink;
+        private bool isFocused = false;
+        //Private methods
+        private void UpdateControlHeight()
+        {
+            if (textBox1.Multiline == false)
+            {
+                int txtHeight = TextRenderer.MeasureText("Text", this.Font).Height + 1;
+                textBox1.Multiline = true;
+                textBox1.MinimumSize = new Size(0, txtHeight);
+                textBox1.Multiline = false;
+
+                this.Height = textBox1.Height + this.Padding.Top + this.Padding.Bottom;
+            }
+        }
+
+        //Properties
+        [Category("FemtoHelp")]
+        public Color BorderColor
+        {
+            get { return borderColor; }
+            set
+            {
+                borderColor = value;
+                this.Invalidate();
+            }
+        }
+        [Category("FemtoHelp")]
+        public int BorderSize
+        {
+            get { return borderSize; }
+            set
+            {
+                borderSize = value;
+                this.Invalidate();
+            }
+        }
+
+        [Category("FemtoHelp")]
+        public bool UnderlinedStyle
+        {
+            get { return underlinedStyle; }
+            set
+            {
+                underlinedStyle = value;
+                this.Invalidate();
+            }
+        }
+
+        [Category("FemtoHelp")]
+        public bool PasswordChar
+        {
+            get { return textBox1.UseSystemPasswordChar; }
+            set { textBox1.UseSystemPasswordChar = value; }
+        }
+
+        [Category("FemtoHelp")]
+        public bool Multiline
+        {
+            get { return textBox1.Multiline; }
+            set { textBox1.Multiline = value; }
+        }
+
+        [Category("FemtoHelp")]
+        public override Color BackColor
+        {
+            get { return base.BackColor; }
+            set
+            {
+                base.BackColor = value;
+                textBox1.BackColor = value;
+            }
+        }
+
+        [Category("FemtoHelp")]
+        public override Color ForeColor
+        {
+            get { return base.ForeColor; }
+            set
+            {
+                base.ForeColor = value;
+                textBox1.ForeColor = value;
+            }
+        }
+
+        [Category("FemtoHelp")]
+        public override Font Font
+        {
+            get { return base.Font; }
+            set
+            {
+                base.Font = value;
+                textBox1.Font = value;
+                if (this.DesignMode)
+                    UpdateControlHeight();
+            }
+        }
+
+        [Category("FemtoHelp")]
+        public string Texts
+        {
+            get { return textBox1.Text; }
+            set { textBox1.Text = value; }
+        }
+
+        [Category("FemtoHelpe")]
+        public Color BorderFocusColor
+        {
+            get { return borderFocusColor; }
+            set { borderFocusColor = value; }
+        }
+
+        //Overridden methods
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics graph = e.Graphics;
+
+            //Draw border
+            using (Pen penBorder = new Pen(borderColor, borderSize))
+            {
+                penBorder.Alignment = System.Drawing.Drawing2D.PenAlignment.Inset;
+                if (isFocused) penBorder.Color = borderFocusColor;//Set Border color in focus. Otherwise, normal border color
+
+                if (underlinedStyle) //Line Style
+                    graph.DrawLine(penBorder, 0, this.Height - 1, this.Width, this.Height - 1);
+                else //Normal Style
+                    graph.DrawRectangle(penBorder, 0, 0, this.Width - 0.5F, this.Height - 0.5F);
+            }
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            if (this.DesignMode)
+                UpdateControlHeight();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            UpdateControlHeight();
+        }
+        //Change border color in focus mode
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            isFocused = true;
+            this.Invalidate();
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            isFocused = false;
+            this.Invalidate();
+        }
+        //Default Event
+        public event EventHandler _TextChanged;
+        //TextBox-> TextChanged event
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (_TextChanged != null)
+                _TextChanged.Invoke(sender, e);
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            this.OnClick(e);
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            this.OnKeyPress(e);
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            this.OnKeyDown(e);
+        }
+
+        private void textBox1_DoubleClick(object sender, EventArgs e)
+        {
+            this.OnDoubleClick(e);
+        }
+
+        private void textBox1_FontChanged(object sender, EventArgs e)
+        {
+            this.OnFontChanged(e);
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            this.OnKeyUp(e);
+
+        }
+
+        private void textBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.OnMouseClick(e);
+        }
+
+        private void textBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.OnMouseDoubleClick(e);
+        }
+
+        private void textBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.OnMouseDown(e);
+        }
+
+        private void textBox1_MouseHover(object sender, EventArgs e)
+        {
+            this.OnMouseHover(e);
+        }
+
+        private void textBox1_MouseLeave(object sender, EventArgs e)
+        {
+            this.OnMouseLeave(e);
+        }
+
+        private void textBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.OnMouseMove(e);
+        }
+
+        private void textBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.OnMouseUp(e);
+        }
+
+        private void textBox1_Move(object sender, EventArgs e)
+        {
+            this.OnMove(e);
+        }
+    }
+}
